64d6c8a1d75d2a307a724e9a85f9b9eeec4f79c7
Yusinto Ngadiman <yusinto@gmail.com>
2024-09-05 00:15:39 -0700

    feat: Installed react sdk. Added LDProvider and useFlag.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LDProvider } from 'launchdarkly-react-client-sdk'
 import App from './App.tsx'
 import './index.css'
 
+const clientSideID = import.meta.env.VITE_CLIENT_SIDE_ID ?? ''
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LDProvider clientSideID={clientSideID}>
+      <App />
+    </LDProvider>
   </StrictMode>,
 )
